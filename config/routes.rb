51a5fddde4a81c36eb6c1d93c2bbb1ca17c79efa
Owen Klumb <owen.klumb@gmail.com>
2017-07-04 16:13:06 +0900

    login complete.  Ternary operators FTW

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :articles
   get '/', to: 'articles#index'
+  get '/signup' => 'users#new'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  post '/users' => 'users#create'
+
 end
